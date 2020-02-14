b1ed2f012c3fc235fc0ffdc235fe080ad42ffa7b
Walee <waleemamun@gmail.com>
2020-02-14 17:13:45 -0500

    rec

--- src/Recursions.java
+++ src/Recursions.java
@@ -9,6 +9,11 @@ public class Recursions {
             return countWays(n-1) + countWays(n-2) + countWays(n-3);
 
     }
+    /*
+     *  Count for 1 steps 2 steps and 3 steps - the stairs
+     *  as the previous position already accounted for n-1, n-2 n-3 steps
+     *  at each position x we store the ways to climb ways[x] using the last 3 steps
+     * */
     public int countWaysDP(int n) {
         int [] ways = new int[n+1];
         ways[0] = 0;
@@ -19,5 +24,21 @@ public class Recursions {
             ways[i] = ways[i-1] + ways[i-2] + ways[i-3];
         }
         return ways[n];
+    }
+    public int getMagic(int arr[], int low, int high) {
+        int mid = (low + high) / 2;
+        if (low > high)
+            return -1;
+        if (arr[mid] == mid)
+            return mid;
+        else if (arr[mid] > mid)
+            return getMagic(arr, low, mid-1);
+        else
+            return getMagic(arr, mid+1, high);
+
+    }
+    public int magicShow(int arr[]) {
+        int mid = getMagic(arr,0,arr.length-1);
+        return mid;
     }
 }
