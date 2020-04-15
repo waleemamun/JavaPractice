78e8e762c5ef3101ec697862b8e6147a98ce3d2e
Walee <wmamun@cisco.com>
2020-04-14 20:10:08 -0400

    LeetCode :: 54 Spiral Matrix

--- src/SolutionsV1.java
+++ src/SolutionsV1.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SolutionsV1 {
     // Commpare two double numbers
     public int compareDoubles(String version1, String version2) {
@@ -72,4 +75,28 @@ public class SolutionsV1 {
         }
         return  maxSum;
     }
-}
+
+    // Leetcode :: 54 Spiral Matrix
+    // The answer will be all the elements in clockwise order from the first-outer layer,
+    // followed by the elements from the second-outer layer, and so on.
+    public List< Integer > spiralOrder(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0)
+            return ans;
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
+            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
+                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+
+    }
