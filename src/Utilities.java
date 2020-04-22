7f14c95d4cdfa20658f295beef60e62682b8d04d
Walee <wmamun@cisco.com>
2020-04-22 16:48:39 -0400

    utility

--- src/Utilities.java
+++ src/Utilities.java
@@ -1,5 +1,7 @@
 public class Utilities {
 
+    // Remember to call with high == arr.length - 1 as high is
+    // the highest index of the array not the length
     public static int binSearch (int []arr, int low, int high, int value) {
         if (low > high)
             return -1;
@@ -15,8 +17,43 @@ public class Utilities {
         }
     }
 
-    public static int binSearchNext(int []arr, int low, int high, int value) {
-        return 0;
+    // Remember to call with high == arr.length - 1 as high is
+    // the highest index of the array not the length
+    public static int binSearchPos(int []arr, int low, int high, int value) {
+        if(low > high) {
+            if (low == arr.length)
+                return low;
+            return high;
+        }
+
+        int mid = (low + high)/2;
+        //System.out.println(low + " " + mid + " " + high);
+        if(value == arr[mid]) {
+            return mid;
+        } else if (value < arr[mid]) {
+            return binSearchPos(arr, low, mid -1, value);
+        } else
+            return binSearchPos(arr,mid+1, high, value);
+
+    }
+    // Remember to call with high == arr.length - 1 as high is
+    // the highest index of the array not the length
+    public static int binSearcPos2D (int interval[][], int low, int high, int value) {
+        if(low > high) {
+            if (low == interval.length)
+                return low;
+            return high;
+        }
+        int mid = (low + high)/2;
+        if (value == interval[mid][0]) {
+            return mid;
+        } else if (value < interval[mid][0]) {
+            return binSearcPos2D(interval, low, mid-1, value);
+        } else
+            return binSearcPos2D(interval, mid + 1, high, value);
+
+
+
     }
     private static int makeArrayCopy (int nums[] , int buffer[], int start, int end) {
         int entry = 0;
