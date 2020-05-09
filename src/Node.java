bd8c20102b157941d296d505604839983eb950c8
Walee <wmamun@cisco.com>
2020-05-09 17:41:44 -0400

    changes

--- src/Node.java
+++ src/Node.java
@@ -1,9 +1,24 @@
+import java.util.List;
+
 public class Node {
     int val;
     int min;
 
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
     public Node(int v, int m) {
         val = v;
         min = m;
     }
+
 }
